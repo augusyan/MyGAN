ea86440e5e346059b4c379819e780b8685373638
augusyan <killbill4@163.com>
2019-07-16 14:55:15 +0800

    pycharm test

---
+++ connect_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19-7-16 下午2:54
+# @Author  : Yan
+# @Site    : 
+# @File    : connect_test.py
+# @Software: PyCharm
+# @Function: 
+# @update:
+
+# just test everything
